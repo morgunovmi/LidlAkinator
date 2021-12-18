8c4597788717335b6f5ca28fae3ed5252bcb17a2
Max Morgunov <max@pop-os.localdomain>
2021-12-19 01:49:32 +0300

    Moved arg parsing to a header file

---
+++ src/argParser.h
@@ -0,0 +1,61 @@
+#ifndef ARGPARSER_H
+#define ARGPARSER_H
+
+#include <argp.h> 
+
+const char *argp_program_bug_address = "<maxexmachina@gmail.com>";
+
+static char doc[] = "A lidl akinator for you command line.";
+
+static struct argp_option options[] = {
+    {"say", 's', 0, 0, "Use text to speech"},
+    {"debug", 'd', 0, 0, "Makes tree dump debug friendly"},
+    {"file", 'f', "FILE_PATH", 0, "Use a custom file for the database"}
+};
+
+struct arguments {
+    int say, debug;
+    char *dbFilePath;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = (struct arguments *)state->input;
+
+    switch (key) {
+        case 's':
+            arguments->say = 1;
+            break;
+        case 'd':
+            arguments->debug = 1;
+            break;
+        case 'f':
+            arguments->dbFilePath = arg;
+            break;
+        case ARGP_KEY_ARG:
+            argp_usage(state);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, 0, doc };
+
+static void parseArgs(int argc, char **argv, bool *say, bool *debug, char **dbPath) {
+    struct arguments arguments = {};
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    if (arguments.say) {
+        *say = true;
+    }
+
+    if (arguments.debug) {
+        *debug = true;
+    }
+
+    *dbPath = arguments.dbFilePath;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,71 +5,23 @@
 #include "tree.h"
 #include "akinator.h"
 #include "../include/text.h"
+#include "argParser.h"
 
 extern bool ifSay;
 extern bool ifDebug;
 
 //TODO ifSay other way
-//TODO different tree dump modes through cl flags
-//TODO Exit dump mode by key
-
-const char *argp_program_bug_address = "<maxexmachina@gmail.com>";
-
-static char doc[] = "A lidl akinator for you command line.";
-
-static struct argp_option options[] = {
-    {"say", 's', 0, 0, "Use text to speech"},
-    {"debug", 'd', 0, 0, "Makes tree dump debug friendly"},
-    {"file", 'f', "FILE_PATH", 0, "Use a custom file for the database"}
-};
-
-struct arguments {
-    int say, debug;
-    char *dbFilePath;
-};
-
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    struct arguments *arguments = (struct arguments *)state->input;
-
-    switch (key) {
-        case 's':
-            arguments->say = 1;
-            break;
-        case 'd':
-            arguments->debug = 1;
-            break;
-        case 'f':
-            arguments->dbFilePath = arg;
-            break;
-        case ARGP_KEY_ARG:
-            argp_usage(state);
-            break;
-        default:
-            return ARGP_ERR_UNKNOWN;
-    }
-    return 0;
-}
-
-static struct argp argp = { options, parse_opt, 0, doc };
 
 int main(int argc, char **argv) {
-    struct arguments arguments = {};
+    char *dbFilePath = "";
 
-    argp_parse(&argp, argc, argv, 0, 0, &arguments);
-
-    if (arguments.say) {
-        ifSay = true;
-    }
-
-    if (arguments.debug) {
-        ifDebug = true;
-    }
+    parseArgs(argc, argv, &ifSay, &ifDebug, &dbFilePath);
 
     Tree *tree = nullptr;
     text_t text = {};
 
-    if (arguments.dbFilePath) {
-        if (getText(arguments.dbFilePath, &text) != 1) {
+    if (dbFilePath) {
+        if (getText(dbFilePath, &text) != 1) {
             return EXIT_FAILURE;
         }
 
